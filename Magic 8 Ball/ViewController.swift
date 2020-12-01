ad7cf0ac46a3a303ccd7fca52f2a54d069403a25
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2020-12-01 18:23:36 +0000

    First Commit
    Magic 8 Ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    var randomBallNumber = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateImage()
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        updateImage()
+    }
+    
+    func updateImage () {
+        
+        randomBallNumber = Int(arc4random_uniform(5)+1)
+        
+        imageView.image = UIImage(named: "ball\(randomBallNumber)")
+    }
+    
 }
 
